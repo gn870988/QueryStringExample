79f6e22251055eb73ac8a7c61e4e874643c3bd2f
余旻澤 <gn870988@gmail.com>
2018-04-30 19:10:38 +0800

    初版

---
+++ QueryStringExample/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+
+namespace QueryStringExample
+{
+    public static class Extensions
+    {
+        public static string ToQueryString(this NameValueCollection nvc)
+        {
+            IEnumerable<string> segments = from key in nvc.AllKeys
+                                           from value in nvc.GetValues(key)
+                                           select string.Format("{0}={1}",
+                                           WebUtility.UrlEncode(key),
+                                           WebUtility.UrlEncode(value));
+
+            return "?" + string.Join("&", segments);
+        }
+
+    }
+}
--- QueryStringExample/Program.cs
+++ QueryStringExample/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
 namespace QueryStringExample
 {
@@ -10,6 +9,49 @@ namespace QueryStringExample
     {
         static void Main(string[] args)
         {
+            string url = $"https://www.google.com.tw";
+
+            var nvc = new NameValueCollection
+            {
+                {"parameter1", "1" },
+                {"parameter2", "two" },
+                {"parameter3", "中文" }
+            };
+
+            // 可用Add方法加入<string, string>
+            nvc.Add("parameter4", "測試");
+
+            // ***【補充】***
+            // 要在網頁上接受QueryString方法
+            // NameValueCollection nvc = Request.QueryString;
+
+            // 【Demo1】網址 + QueryString參數
+            Console.WriteLine($"-------------目標網址-------------");
+            Console.WriteLine($"{url}{nvc.ToQueryString()}");
+            Console.WriteLine($"---------------------------------");
+            Console.WriteLine();
+
+            // 接收QueryString參數
+            var collection = HttpUtility.ParseQueryString($"{nvc.ToQueryString()}");
+
+            // 拆解QueryString參數
+            var all = from key in nvc.AllKeys
+                      from value in nvc.GetValues(key)
+                      select ($"{key}：{value}");
+
+            // 【Demo2】
+            Console.WriteLine($"-------------解析QueryString全部參數-------------");
+            Console.WriteLine($"{string.Join("，", all)}");
+            Console.WriteLine($"-----------------------------------------------");
+            Console.WriteLine();
+
+            // 【Demo3】
+            Console.WriteLine($"-------------解析QueryString單參數-------------");
+            Console.WriteLine($"{collection.GetKey(0)}：{collection["parameter1"]}");
+            Console.WriteLine($"---------------------------------------------");
+            Console.WriteLine();
+
+            Console.Read();
         }
     }
 }
